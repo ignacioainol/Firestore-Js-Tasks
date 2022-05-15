797b825fca2d5e677e667b3de47217a7777b33fd
Ignacio Ainol <iggnaxios@gmail.com>
2022-05-15 13:42:14 -0400

    first commit

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8HPnYcVrEyP7Evt2DlJkM0Z0AdSrbuSM",
+  authDomain: "crud-db-vue.firebaseapp.com",
+  databaseURL: "https://crud-db-vue-default-rtdb.firebaseio.com",
+  projectId: "crud-db-vue",
+  storageBucket: "crud-db-vue.appspot.com",
+  messagingSenderId: "928012435777",
+  appId: "1:928012435777:web:0709e9658e9a2b794ee1c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+export const saveTask = (title, description) => {
+  addDoc(collection(db, 'tasks'), { title, description });
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+import { saveTask } from "./firebase.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+});
+
+const taskForm = document.getElementById('task-form');
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = taskForm['task-title'];
+  const description = taskForm['task-description'];
+
+  saveTask(title.value, description.value);
+  taskForm.reset();
+});
